3a642cfe42d514baddb82ead1fcc99295f3d3754
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-05-10 18:39:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const URL = 'https://kodaktor.ru/j/users';
 const app = express();
 app
  .get(/hello/, r => r.res.end('Hello world!'))
+
 /* .get(/users/, async r => {
    const { data: { users: items } } = await get(URL);
    r.res.render('list', { title: 'Список логинов', items }); */
@@ -15,16 +16,16 @@ app
 
  .get('/aprilusers', async r => {
     const  items = await User.find();
-    r.res.render('list', { title: 'Список логинов из БД', items });
+    r.res.render('list', { title: 'Список логинов', items });
 
 })
 
 .get('/aprilusers/:login', async r => {
    const  item = await User.findOne({"login": r.params.login});
-    r.res.send('<h3>Password of ' + r.params.login + ' is:</h3>' + item.password);
+    r.res.send('<h3>Пароль пользователя <b> ' + r.params.login + '</b>:</h3>' + item.password);
 })
 
- .get('/', r => r.res.send('HOME'))
+ .get('/', r => r.res.send('Добро пожаловать!'))
  .use(r => r.res.status(404).end('Still not here, sorry!'))
  .use((e,r,res,n) => res.status(500).end(`Error: ${e}`))
  .set('view engine', 'pug')
